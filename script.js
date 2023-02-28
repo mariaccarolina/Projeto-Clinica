50c0283f9bc589a3ed9f06c3d1e4dfd0ed5af641
Maria Carolina <demycar24@gmail.com>
2023-02-28 15:50:46 -0300

    exames medicos

--- script.js
+++ script.js
@@ -11,7 +11,40 @@ function adcNavegacao() {
 
 const voltar = document.querySelector(".fechar");
 
-voltar.addEventListener("click", fecharNavegacao)
+voltar.addEventListener("click", fecharNavegacao);
 function fecharNavegacao() {
-    nav.style.display = "none";
+  nav.style.display = "none";
 }
+const controls = document.querySelectorAll(".control");
+let currentItem = 0;
+const items = document.querySelectorAll(".item");
+const maxItems = items.length;
+
+controls.forEach((control) => {
+  control.addEventListener("click", (e) => {
+    isLeft = e.target.classList.contains("arrow-left");
+
+    if (isLeft) {
+      currentItem -= 1;
+    } else {
+      currentItem += 1;
+    }
+
+    if (currentItem >= maxItems) {
+      currentItem = 0;
+    }
+
+    if (currentItem < 0) {
+      currentItem = maxItems - 1;
+    }
+
+    items.forEach((item) => item.classList.remove("current-item"));
+
+    items[currentItem].scrollIntoView({
+      behavior: "smooth",
+      inline: "center"
+    });
+
+    items[currentItem].classList.add("current-item");
+  });
+});
